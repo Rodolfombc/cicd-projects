f07f711e85ce70a8d19fb560b6e78ab9831591c1
Rodolfo Machado Brandao Costa <rodolfo.costa@amedigital.com>
2022-01-25 16:34:00 -0300

    Adding my-java-gradle-app sample project

---
+++ my-java-gradle-app/src/test/java/com/example/myjavagradleapp/MyJavaGradleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myjavagradleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJavaGradleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
